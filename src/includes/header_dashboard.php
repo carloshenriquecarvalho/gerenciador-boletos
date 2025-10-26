394fa8f68f540f342e3a3c66a5b36208341c2d61
carlos <misterchaty@outlook.com>
2025-10-26 13:07:16 -0300

    create the dashboard header

--- src/includes/header_dashboard.php
+++ src/includes/header_dashboard.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= isset($titulo) ? htmlspecialchars($titulo) : 'Titulo padrao' ?></title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <header>
-        <nav><p><a href="../src/actions/logout.php">Sair</a></p></nav>
+        <img src="./img/logo_trial.png" alt="logo de boletos verde com vermelho">
+        <nav>
+            <ul>
+                <li><a href="../src/actions/logout.php">Sair</a></li>
+            </ul>
+        </nav>
     </header>
     <main>
