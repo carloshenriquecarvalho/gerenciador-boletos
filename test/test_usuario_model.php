e9c75bb92a3263d68e755207dbc036687751056b
carlos <misterchaty@outlook.com>
2025-11-06 21:17:40 -0300

    Add tests. I tested all of my classes

--- test/test_usuario_model.php
+++ test/test_usuario_model.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/src/model/Usuario.php';
 
-$p = new Usuario("carlos@gmail.com", "carlos");
+$p = new Usuario("carlos@gmail.com", "carlos", null);
 
 echo "<p><strong>Nome:</strong> {$p->getNome()} <br>";
 echo "<p><strong>Email:</strong> {$p->getEmail()} </p>";
