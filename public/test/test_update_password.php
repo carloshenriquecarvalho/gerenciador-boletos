1d2cfb26c6c7c2d44e538fd43fbd59ffe0f4fb50
carlos <misterchaty@outlook.com>
2025-11-08 14:05:47 -0300

    Restup project

--- public/test/test_update_password.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/src/Database.php';
-require_once __DIR__ . '/src/repository/UsuarioRepository.php';
-$database = new Database();
-$pdo = $database->getConnection();
-$repo = new UsuarioRepository($pdo);
-
-$repo->updatePassword(5, "minha_senha_segura123", "09010237");
-?>
--- public/test/test_usuario_model.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/src/model/Usuario.php';
-
-$p = new Usuario("carlos@gmail.com", "carlos", null);
-
-echo "<p><strong>Nome:</strong> {$p->getNome()} <br>";
-echo "<p><strong>Email:</strong> {$p->getEmail()} </p>";
-?>
